44dee8f5a4e9a216fab2d46d4bce7b73baa00e5a
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2022-10-24 23:26:34 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { isLoggedInVar } from "./apollo";
+import { isLoggedInVar, darkModeVar } from "./apollo";
 import { useReactiveVar } from "@apollo/client";
+import { ThemeProvider } from "styled-components";
+
+const lightTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "lightgray",
+};
+
+const darkTheme = {
+  fontColor: "lightgray",
+  bgColor: "#2c2c2c",
+};
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
 
   return (
     <Router>
