375514c149474538826e92767896fe0076c48300
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2022-11-03 22:01:55 +0900

    react hook form 2

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,6 +9,7 @@ import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
 import Button from "../components/auth/Button";
 import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
 import Input from "../components/auth/Input";
 import Separator from "../components/auth/Separator";
 import PageTitle from "../components/PageTitle";
@@ -22,12 +23,14 @@ const FacebookLogin = styled.div`
 `;
 
 function Login() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ mode: "onBlur" });
+
   const onSubmitValid = (data) => {
-    console.log(data);
-  };
-  const onSubmitInvalid = (data) => {
-    console.log(data, "invalid");
+    // console.log(data);
   };
 
   return (
@@ -38,23 +41,29 @@ function Login() {
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
 
-        <form onSubmit={handleSubmit(onSubmitValid, onSubmitInvalid)}>
+        <form onSubmit={handleSubmit(onSubmitValid)}>
           <Input
             type="text"
             placeholder="Username"
             {...register("username", {
               required: "Username is required",
-              minLength: 5,
+              minLength: {
+                value: 5,
+                message: "Username should be longer than 5",
+              },
             })}
             name="username"
+            hasError={Boolean(errors?.username?.message)}
           />
+          <FormError message={errors?.username?.message} />
           <Input
             type="password"
             placeholder="Password"
             name="password"
             {...register("password", { required: true })}
+            hasError={Boolean(errors?.password?.message)}
           />
-
+          <FormError message={errors?.password?.message} />
           <Button type="submit" value="Log in" />
         </form>
         <Separator />
